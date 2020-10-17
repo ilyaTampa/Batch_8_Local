aa8a5af0c6e365cf0f745766c0ef313374415a2f
ilyaTampa <email4ilya@yahoo.com>
2020-10-17 14:56:58 -0400

    While Loop and DoWhileLoop

---
+++ src/com/syntax/class07/LuckyNumber.java
@@ -0,0 +1,27 @@
+package com.syntax.class07;
+
+import java.util.Scanner;
+
+public class LuckyNumber {
+
+	public static void main(String[] args) {
+		
+		        // play a lottery where we need to enter number from 1 to 100
+				// lucky number is 7
+				// keep asking a user a number until entered number is same as lucky number
+
+		Scanner scan;
+		int numberFromUser;
+		
+		int luckyNumber=7;
+		
+		scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("Please enter any number from range 1 to 100");
+			numberFromUser=scan.nextInt();
+		}while(numberFromUser!=luckyNumber);
+		System.out.println("You are the Winner!!!");
+	}
+
+}
